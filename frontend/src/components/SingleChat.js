ec7ef580e2757f246eec91757a9fa6a146150a75
zjkumar <zjagadeeshkumar@gmail.com>
2024-10-14 06:23:22 +0530

    updated backend url

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -22,8 +22,7 @@ const defaultOptions = {
     },
   };
 
-
-const ENDPOINT = "http://localhost:5000"
+const ENDPOINT = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000';
 var socket, selectedChatCompare;
 
 const SingleChat = ({fetchAgain, setFetchAgain}) => {
